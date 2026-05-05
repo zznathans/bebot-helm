72db9d94615262fd5bf1f949e2c27b5f20986398
nathan <nathan@zswap.net>
2026-05-05 13:35:26 -0400

    fix(ci): hard shutdown

--- charts/bebot/docker-entrypoint.sh
+++ charts/bebot/docker-entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/ash
 # shellcheck shell=dash
-EXITCODE=255
-while [ "$EXITCODE" -eq 255 ]; do
-        /usr/bin/php StartBot.php "$@" &
-        PHP_PID=$!
-        # Forward SIGTERM to the PHP process and stop restarting.
-        trap 'kill "$PHP_PID"; EXITCODE=0' TERM
-        wait "$PHP_PID"
-        EXITCODE=$?
-        trap - TERM
-done
-exit $EXITCODE
+/usr/bin/php StartBot.php "$@" &
+PHP_PID=$!
+trap 'kill "$PHP_PID"' TERM
+wait "$PHP_PID"
+exit $?
