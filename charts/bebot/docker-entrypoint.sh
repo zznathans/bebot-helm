cc20f295c3ff1cf9f997344bcf34b3f2727c935c
nathan <nathan@zswap.net>
2026-05-05 13:26:32 -0400

    fix(helm): pvc adjustments

--- charts/bebot/docker-entrypoint.sh
+++ charts/bebot/docker-entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/ash
 # shellcheck shell=dash
-errorMessage() {
-        echo "$*"
-        exit 1
-}
 EXITCODE=255
 while [ "$EXITCODE" -eq 255 ]; do
-        trap "" TERM
-        # shellcheck disable=SC2086
-        /usr/bin/php  StartBot.php "$@"
+        /usr/bin/php StartBot.php "$@" &
+        PHP_PID=$!
+        # Forward SIGTERM to the PHP process and stop restarting.
+        trap 'kill "$PHP_PID"; EXITCODE=0' TERM
+        wait "$PHP_PID"
         EXITCODE=$?
         trap - TERM
 done
